4dd7312578658bc7558dd1ef0e0bc3044184146e
hpoggie <henry.poggie@gmail.com>
2018-12-24 11:10:39 -0500

    Squashed 'ul_core/' changes from 20219d1..54b4715
    54b4715 Revert "CardIds are set by faction"
    
    git-subtree-dir: ul_core
    git-subtree-split: 54b4715f5194cfcf9b44b8555074aeb7461f25c2

--- factions/__init__.py
+++ factions/__init__.py
@@ -13,11 +13,7 @@ availableFactions = [
 
 allCards = {}
 
-i = 0
-
 for module in (base, templars, mariners, thieves, fae):
     for card in module.allCards:
         allCards[card.__name__] = card
         card.iconPath = module.iconPath
-        card.cardId = i
-        i += 1
