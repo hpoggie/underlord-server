42d35e5682d7195d6d1a7b71bebe4ce1e04916c1
Henry Poggie <henry.poggie@gmail.com>
2019-05-25 19:39:01 -0400

    Handle animations

--- server_event_handler.py
+++ server_event_handler.py
@@ -1,16 +1,5 @@
 from ul_core.core.event_handler import EventHandler
 from ul_core.net.network import ClientNetworkManager
-from ul_core.net.zie import gameEntityToZie
-
-from conversions import getCard
-
-
-def playAnimation(conn, name, *args):
-    conn.playAnimation(getattr(ClientNetworkManager.Animations, name), *args)
-
-
-def expandTargets(pl, targets):
-    return (i for t in targets for i in gameEntityToZie(pl, t))
 
 
 class ServerEventHandler(EventHandler):
@@ -20,16 +9,16 @@ class ServerEventHandler(EventHandler):
 
     def on_spawn(self, card):
         for c in self.connections:
-            playAnimation(c, 'on_spawn', card.zone.index(card))
+            c.playAnimation('on_spawn', card)
 
     def on_fight(self, c1, c2):
         for conn in self.connections:
             pl = conn.player
-            playAnimation(conn, 'on_fight', *(gameEntityToZie(pl, c1) + gameEntityToZie(pl, c2)))
+            conn.playAnimation('on_fight', c1, c2)
 
     def on_die(self, card):
         for c in self.connections:
-            playAnimation(c, 'on_die', card.zone.index(card))
+            c.playAnimation('on_die', card)
 
     # TODO
     def on_change_controller(self, card, original, new):
@@ -41,18 +30,16 @@ class ServerEventHandler(EventHandler):
     def on_reveal_facedown(self, card, targets):
         for c in self.connections:
             pl = c.player
-            playAnimation(c, 'on_reveal_facedown', card.zone.index(card),
-                          *expandTargets(pl, targets))
+            c.playAnimation('on_reveal_facedown', card, targets)
 
     def on_play_faceup(self, card, targets):
         for c in self.connections:
             pl = c.player
-            playAnimation(c, 'on_play_faceup', card.zone.index(card),
-                          *expandTargets(pl, targets))
+            c.playAnimation('on_play_faceup', card, targets)
 
     def on_play_facedown(self, card):
         # TODO: do this for both players
-        playAnimation(card.owner.connection, 'on_play_facedown', *getCard(card.owner, card))
+        card.owner.connection.playAnimation('on_play_facedown', card)
         #playAnimation(self.connections[card.owner], 'on_play_facedown_invisible')
         pass
 
@@ -63,4 +50,4 @@ class ServerEventHandler(EventHandler):
 
     def on_end_turn(self, game):
         for c in self.connections:
-            playAnimation(c, 'on_end_turn')
+            c.playAnimation('on_end_turn')
